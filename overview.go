e44cc906650f6c30e0afa021571a3eb44bc05026
Andy Kreling <krelinga@gmail.com>
2025-06-07 16:09:23 +0000

    add reflectnotes to global spelling ignore list.

--- overview.go
+++ overview.go
@@ -4,4 +4,4 @@
 // The package is structured into a few top-level functions that are like section headers.
 // Each function has some comments explaining the concepts, and links to some example to
 // demonstrate the concepts in action.
-package reflectnotes // spell-checker:ignore reflectnotes
+package reflectnotes
